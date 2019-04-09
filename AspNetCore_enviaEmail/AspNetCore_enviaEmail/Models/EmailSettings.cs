fe6c17c286ea2a6cebdf5fbdf3215d0fd5b0c9d1
vitoraugustoa <vitoraugustoa@hotmail.com>
2019-04-09 16:54:29 -0300

    Enviando e-email via SMTP .net core

---
+++ AspNetCore_enviaEmail/AspNetCore_enviaEmail/Models/EmailSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace AspNetCore_enviaEmail.Models
+{
+    public class EmailSettings
+    {
+        public Host Hosts { get; set; }
+        public int Port { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+        public string FromEmail { get; set; }
+        public string ToEmail { get; set; }
+        public string CcEmail { get; set; }
+
+    }
+
+    public class Host
+    {
+        public string Gmail { get; set; }
+        public string Hotmail { get; set; }
+        public string Yahoo { get; set; }
+        
+    }
+}
