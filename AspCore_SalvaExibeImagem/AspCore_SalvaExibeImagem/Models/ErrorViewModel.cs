72f867ffc0e83dfdbfb2e18c60abb5a0400eed2c
vitoraugustoa <vitoraugustoa@hotmail.com>
2019-04-10 17:27:57 -0300

    Trabalhando com Imagens .net core

---
+++ AspCore_SalvaExibeImagem/AspCore_SalvaExibeImagem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspCore_SalvaExibeImagem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
