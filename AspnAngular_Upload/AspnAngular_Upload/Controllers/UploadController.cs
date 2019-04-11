eebdb2da781770335a59e61bc65c61377a4ba5fe
vitoraugustoa <vitoraugustoa@hotmail.com>
2019-04-11 14:12:34 -0300

    Upload de arquivos Angular e .net core

---
+++ AspnAngular_Upload/AspnAngular_Upload/Controllers/UploadController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net.Http.Headers;
+
+namespace AspnAngular_Upload.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class UploadController : Controller
+    {
+        private IHostingEnvironment _hostingEnvironment;
+        public UploadController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                string nomePastaServidor = "Arquivos";
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                string caminhoPastaServidor = Path.Combine(webRootPath, nomePastaServidor);
+                if (!Directory.Exists(caminhoPastaServidor))
+                {
+                    Directory.CreateDirectory(caminhoPastaServidor);
+                }
+                if (file.Length > 0)
+                {
+                    string nomeArquivo = 
+                        ContentDispositionHeaderValue.Parse(file.ContentDisposition)
+                        .FileName.Trim('"');
+                    string caminhoCompleto = Path.Combine(caminhoPastaServidor, nomeArquivo);
+                    using (var stream = new FileStream(caminhoCompleto, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+                return Json("Arquivo enviado com SUCESSO !!!.");
+            }
+            catch (System.Exception ex)
+            {
+                return Json("O envio do arquivo FALHOU : " + ex.Message);
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
