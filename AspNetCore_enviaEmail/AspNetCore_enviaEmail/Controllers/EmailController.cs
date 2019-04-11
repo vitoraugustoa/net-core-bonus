52872bd1e37fa36cd00bd2696bc3e099f26f6988
vitoraugustoa <vitoraugustoa@hotmail.com>
2019-04-11 14:10:31 -0300

    Enviando Email via SMTP .net core

---
+++ AspNetCore_enviaEmail/AspNetCore_enviaEmail/Controllers/EmailController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore_enviaEmail.Services.Interfaces;
+using AspNetCore_enviaEmail.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_enviaEmail.Controllers
+{
+    public class EmailController : Controller
+    {
+        private readonly IEmailSender _emailSender;
+        public EmailController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EnviarEmail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EnviarEmail(EmailViewModel email)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _emailSender.SendEmailAsync(email.Destino , email.Assunto , email.Mensagem);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception)
+                {
+                    return RedirectToAction(nameof(EmailFalha));
+                }
+            }
+            return View(email);
+        }
+
+        [HttpGet]
+        public IActionResult EmailFalha()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ AspNetCore_enviaEmail/AspNetCore_enviaEmail/ViewModels/EmailViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore_enviaEmail.ViewModels
+{
+    public class EmailViewModel
+    {
+        [Required]
+        [Display(Name ="Email de destino"), EmailAddress]
+        public string Destino { get; set; }
+
+        [Required]
+        [Display(Name = "Assunto")]
+        public string Assunto { get; set; }
+
+        [Required]
+        [Display(Name = "Mensagem")]
+        public string Mensagem { get; set; }
+    }
+}
